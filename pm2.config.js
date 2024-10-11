af6ac686545f572bd6c9fe208d1da5ca384a2cdd
neki-dev <dev@neki.guru>
2024-10-12 00:31:15 +0200

    fix(pm2): fix config

--- pm2.config.js
+++ pm2.config.js
@@ -1,29 +1,13 @@
 module.exports = {
   apps: [
     {
-      /**
-       * General
-       */
       name: "dothree",
       script: "./server.js",
       max_memory_restart: "512M",
-
-      /**
-       * Environment
-       */
       env: {
         NODE_ENV: "production",
         PORT: 4801,
       },
-
-      /**
-       * Logs
-       */
-      out_file: "./stdout.log",
-      error_file: "./stderr.log",
-      merge_logs: true,
-      log_date_format: "DD.MM.YYYY HH:mm:ss Z",
-      log_type: "json",
     },
   ],
 };
